c4de7800a7739cb0cde57ff0c63ad54503a0f178
Réal <rgoddat@gmail.com>
2018-06-13 23:17:45 +0200

    Added menu scene

---
+++ Assets/Scripts/ObjectDestroyer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Destroy the gameobject that pass the trigger
+/// </summary>
+public class ObjectDestroyer : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+}
